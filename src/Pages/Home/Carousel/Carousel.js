4237d0a46f1fe1b8853e33cb90e624740880ab9d
Hayat6488 <abulhayat619@gmail.com>
2022-11-09 01:21:04 +0600

    carousel added

--- src/Pages/Home/Carousel/Carousel.js
+++ src/Pages/Home/Carousel/Carousel.js
@@ -7,12 +7,35 @@ import img3 from '../../../Assets/Carousel/buffet-indian-food-spices.jpg'
 const Carousel = () => {
     return (
         <div>
-            <div className="carousel w-full carousel-custom">
+            <div className="carousel w-full carousel-custom mt-10">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img src={img1} alt="" className="w-full h-full"/>
+                    <img src={img1} alt="" className="w-full h-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-20 top-1/2">
+                        <h1 className='text-4xl text-white text-left'>Dhekur is a<br />  home made food <br /> delivary service</h1>
+                    </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={img2} alt="" className="w-full h-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-20 top-1/2">
+                        <h1 className='text-4xl text-white text-left'>We try to<br /> provide the best <br /> taste of bengali cuisine</h1>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={img3} alt="" className="w-full h-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-20 top-1/2">
+                        <h1 className='text-4xl text-white text-left'>Taste the best <br /> flavour of South <br /> Asian spices</h1>
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
                     </div>
                 </div>
             </div>
