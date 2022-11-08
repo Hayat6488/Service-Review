1fae0264f0d4c769af61e3b5f43a10e69a873ab1
Hayat6488 <abulhayat619@gmail.com>
2022-11-08 21:47:43 +0600

    router setup completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { routes } from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App dark bg-slate-100">
-      
+    <div className="App dark bg-slate-900">
+      <RouterProvider router={routes}>
+
+      </RouterProvider>
     </div>
   );
 }
