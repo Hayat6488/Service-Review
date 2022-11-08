34942f0096326f4c2bd81f8cb2bcac03cf54ec94
Hayat6488 <abulhayat619@gmail.com>
2022-11-08 22:25:44 +0600

    navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,46 @@
+import { Navbar } from 'flowbite-react';
 import React from 'react';
+import logo from '../../../Assets/logo/logo.png'
 
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <Navbar
+                className='px-8'
+                fluid={true}
+                rounded={true}
+            >
+                <Navbar.Brand href="https://flowbite.com/">
+                    <img
+                        src={logo}
+                        className="mr-3 h-6 sm:h-9"
+                        alt="Dhekur Logo"
+                    />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                        DHEKUR
+                    </span>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Navbar.Link
+                        href="/navbars"
+                    >
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        About
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Services
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Pricing
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Contact
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     );
 };
