6c623f71ee448f17018fdc9c5b2ad60ae267b509
Hayat6488 <abulhayat619@gmail.com>
2022-11-10 18:22:10 +0600

    user review Edit added

--- src/Pages/UserReviews/UserReview/UserReview.js
+++ src/Pages/UserReviews/UserReview/UserReview.js
@@ -7,16 +7,46 @@ const UserReview = ({ review, forceUpdate }) => {
 
     const handleDelete = id => {
         const proceed = window.confirm('Are you sure you want to delete this review?');
-        if(proceed){
-            fetch(`http://localhost:5000/myreviews/${id}`,{
+        if (proceed) {
+            fetch(`http://localhost:5000/myreviews/${id}`, {
                 method: 'DELETE'
             })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                forceUpdate();
-            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    forceUpdate();
+                })
         }
+    }
+
+    const handleUpdate = event => {
+        event.preventDefault();
+        const form = event.target;
+        const ratings = form.ratings.value;
+        const comment = form.comment.value;
+
+        const updateData = {
+            ratings: ratings,
+            comment: comment
+        }
+
+        fetch(`http://localhost:5000/myreviews/${_id}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(updateData)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.modifiedCount > 0){
+                alert('Edit Successful');
+                event.target.reset();
+                forceUpdate();
+            }
+        })
+
+        
     }
 
     return (
@@ -48,9 +78,8 @@ const UserReview = ({ review, forceUpdate }) => {
                         <FaStar className='mr-1'></FaStar><h2>{ratings}</h2>
                     </div>
                     <div className='mt-2'>
-                        <button className='btn mr-2 btn-outline'>
-                            UPDATE
-                        </button>
+                        <label htmlFor="my-modal-6" className="btn  mr-2 btn-outline">Edit</label>
+
                         <button onClick={() => handleDelete(_id)} className='btn btn-circle btn-outline'>
                             <FaTrashAlt></FaTrashAlt>
                         </button>
@@ -60,6 +89,32 @@ const UserReview = ({ review, forceUpdate }) => {
                     <p className='text-left'>{comment}</p>
                 </div>
 
+            </div>
+
+            {/* Modal Body Part */}
+
+            <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+            <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <form onSubmit={handleUpdate}>
+                        <div className="flex justify-start flex-col">
+                            <label className="label">
+                                <span className="label-text text-xl">Ratings</span>
+                            </label>
+                            <input type="text" defaultValue={ratings} placeholder="Your rating out of 5" className="input rounded-md input-bordered w-1/3" name='ratings' required/>
+                        </div>
+                        <div className="flex justify-start flex-col">
+                            <label className="label">
+                                <span className="label-text text-xl">Comment</span>
+                            </label>
+                            <textarea defaultValue={comment} className="textarea rounded-md textarea-bordered h-24 w-full" placeholder="Your Review" name='comment' required></textarea>
+                        </div>
+                        <button type="submit" className='btn btn-outline btn-success mt-4 px-8 py-2 text-xl'>SUBMIT</button>
+                    </form>
+                    <div className="modal-action">
+                        <label htmlFor="my-modal-6" className="btn btn-success">CLOSE</label>
+                    </div>
+                </div>
             </div>
         </div>
     );
