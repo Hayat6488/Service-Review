e6209a9c391b46598670a46a79b64d37bdb617ea
Hayat6488 <abulhayat619@gmail.com>
2022-11-10 20:17:26 +0600

    Photo view added

--- src/Pages/Foods/Food/Food.js
+++ src/Pages/Foods/Food/Food.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { FaStar } from 'react-icons/fa';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
 
-const Food = ({food}) => {
+const Food = ({ food }) => {
 
     const { img, _id, price, ratings, serviceName, des } = food;
 
     return (
         <div className="card w-96 glass">
-            <figure><img className='h-64 w-full' src={img} alt="" /></figure>
+            <PhotoProvider>
+                <PhotoView src={img}>
+                <img className='h-64 w-full' src={img} alt="" />
+                </PhotoView>
+            </PhotoProvider>
             <div className="card-body">
                 <h2 className="card-title">{serviceName}</h2>
                 <p className='text-left'>{des.slice(0, 100)}...</p>
@@ -19,7 +25,7 @@ const Food = ({food}) => {
                     </div>
                 </div>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary"><Link to={`/foods/${_id}`}>Learn more!</Link></button>
+                    <button className="btn btn-primary"><Link to={`/foods/${_id}`}>DETAILS</Link></button>
                 </div>
             </div>
         </div>
--- src/Pages/Home/FoodMenu/FoodCard/FoodCard.js
+++ src/Pages/Home/FoodMenu/FoodCard/FoodCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { FaStar } from 'react-icons/fa';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const FoodCard = ({ food }) => {
     const { img, _id, price, ratings, serviceName, des } = food;
     return (
         <div className="card w-96 glass">
-            <figure><img className='h-64 w-full' src={img} alt="" /></figure>
+            <PhotoProvider>
+                <PhotoView src={img}>
+                    <img className='h-64 w-full' src={img} alt="" />
+                </PhotoView>
+            </PhotoProvider>
             <div className="card-body">
                 <h2 className="card-title">{serviceName}</h2>
                 <p className='text-left'>{des.slice(0, 100)}...</p>
